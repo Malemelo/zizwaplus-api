f094326938f021c700c01bf666efd7161d4d6baf
imalemelo <imalemelo@yahoo.com>
2021-12-31 17:07:56 +0200

    asdasdA

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/phone-login',[UserController::class,'phoneLoginData']);
+Route::post('/email-login',[UserController::class,'emailLoginData']);
+Route::post('/phone-register',[UserController::class,'phoneRegisterData']);
+Route::post('/email-register',[UserController::class,'emailRegisterData']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
