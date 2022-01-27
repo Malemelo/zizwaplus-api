ae306851cfda6558e2d5aee35134b5116e1f61a3
imalemelo <imalemelo@yahoo.com>
2022-01-28 00:33:14 +0200

    asdasdas

--- app/Models/MtnPaymentIntent.php
+++ app/Models/MtnPaymentIntent.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class MtnPaymentIntent extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'user_id','reference_id','receipt_number','plan_name','plan_amount','plan_currency'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
