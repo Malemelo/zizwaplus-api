7021536c5d67619332f6316144522895e7090821
imalemelo <imalemelo@yahoo.com>
2022-01-27 14:30:03 +0200

    sdasdasd

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -6,6 +6,7 @@ use App\Models\MtnPaymentIntent;
 use App\Models\Payments;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
